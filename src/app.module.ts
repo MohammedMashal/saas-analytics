e15c353237b90b2c2c56b2be3e770bc24121347a
Mohammed Mashal <adelmashal2010@gmail.com>
2025-12-26 21:00:57 +0200

    added configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,30 @@ import { AggregatesModule } from './aggregates/aggregates.module';
 import { JobsModule } from './jobs/jobs.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import envValidation from './config/env.validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: join(process.cwd(), `.env.${process.env.NODE_ENV}`),
+      load: [appConfig, databaseConfig],
+      validationSchema: envValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: true,
+      }),
     }),
     TenantsModule,
     EventsModule,
---
+++ src/config/env.validation.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'test', 'production')
+    .default('development'),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().port().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+});
